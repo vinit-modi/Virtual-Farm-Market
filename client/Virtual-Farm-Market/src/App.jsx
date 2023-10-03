8ff2ffcb2d6cb3fc5eaea919b622a9934da4142f
jeel33 <jmpatel332000@gmail.com>
2023-10-03 10:25:18 -0400

    test

--- client/Virtual-Farm-Market/src/App.jsx
+++ client/Virtual-Farm-Market/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import SignUp from "./pages/Authentication/SignUp";
+import SignInSide from "./pages/Authentication/SignIn";
+import PublicRoute from "./auth/PublicRoute";
+
+import ResetPassword from './pages/Authentication/ResetPassword'
+import IndexForRoutes from './Routes/IndexForRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+   <IndexForRoutes/>
+    
     </>
+    // <div>
+    //   <Switch>
+    //   <Route exact path="/register" component={SignUp}>
+    //   <Route exact path="/login" component={SignInSide} />
+        
+    //     {/* <PublicRoute>
+    //       <Route exact path="/signup"  />
+    //     </PublicRoute> */}
+    //   </Route>
+    // </Switch>
+    // </div>
   )
 }
 
---
+++ client/Virtual-Farm-Market/src/pages/Authentication/SignIn.jsx
@@ -0,0 +1,202 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { IconButton, InputAdornment } from "@mui/material";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required")
+    .matches(
+      /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
+      "Invalid email address"
+    ),
+  password: Yup.string()
+  .required("Required")
+  .min(8, "Must be 8 characters or more")
+  .matches(/[a-z]+/, "One lowercase character")
+  .matches(/[A-Z]+/, "One uppercase character")
+  .matches(/[@$!%*#?&]+/, "One special character")
+  .matches(/\d+/, "One number"),
+});
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      Virtual Farm Market &nbsp;
+      {new Date().getFullYear()}
+      {"."}
+      
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const [isRemember, setIsRemember] = useState(false);
+  console.log(isRemember);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={20}
+          component={Paper}
+          elevation={6}
+          square
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+                remember: false,
+              }}
+              validationSchema={validationSchema}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  console.log(values);
+                  setSubmitting(false);
+                }, 200);
+              }}
+            >
+              {({ isSubmitting, values, handleChange }) => (
+                <Form>
+                  <Field
+                    as={TextField}
+                    margin="normal"
+                    required
+                    variant="outlined"
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                  <ErrorMessage
+                    name="email"
+                    id="email"
+                    component="div"
+                    className="error text-danger"
+                  />
+                  <Field
+                    as={TextField}
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type={!isVisible ? `password` : `text`}
+                    id="password"
+                    autoComplete="current-password"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={() => setIsVisible(!isVisible)}
+                            aria-label="password visibility"
+                            edge="end"
+                          >
+                            {isVisible ? (
+                              <VisibilityIcon />
+                            ) : (
+                              <VisibilityOffIcon />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  <ErrorMessage
+                    name="password"
+                    id="password"
+                    component="div"
+                    className="error text-danger"
+                  />
+
+                  <FormControlLabel
+                    control={
+                      <Field
+                        as={Checkbox}
+                        name="remember"
+                        id="remember"
+                        color="primary"
+                      />
+                    }
+                    label="Remember Me"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    disabled={isSubmitting}
+                  >
+                    Sign In
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+            <Grid container>
+              <Grid item xs>
+                <NavLink to="/forgetpassword" variant="body2">
+                  Forgot password?
+                </NavLink>
+              </Grid>
+              <Grid item>
+                Don't have an account? &nbsp;
+                <NavLink to="/register">{"Sign Up"}</NavLink>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
