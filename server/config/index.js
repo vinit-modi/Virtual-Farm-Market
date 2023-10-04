ce4f7f71514c6da9089e7131e2a9130264f8d52c
vinit-modi <vinitmodi.dev@gmail.com>
2023-10-04 19:56:47 -0400

    MongoDB remote connection

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  MongoDBURL: "mongodb://localhost:27017/VFM",
+  MongoDBURL:
+    "mongodb+srv://vinitmodi:vinit6784@virtual-farm-market.4r7rkrh.mongodb.net/",
   ServerPort: 3001,
 
   secret: "VirtualFarmMarket", // jwt secret key
