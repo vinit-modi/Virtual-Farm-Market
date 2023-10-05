dffc141d6000c468b62e4a7cb1d1df9adf91a3d9
vinit-modi <vinitmodi.dev@gmail.com>
2023-10-04 20:24:19 -0400

    DB name changed

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   MongoDBURL:
-    "mongodb+srv://vinitmodi:vinit6784@virtual-farm-market.4r7rkrh.mongodb.net/",
+    "mongodb+srv://vinitmodi:vinit6784@virtual-farm-market.4r7rkrh.mongodb.net/VirtualFarmMarketDB",
   ServerPort: 3001,
 
   secret: "VirtualFarmMarket", // jwt secret key
