72ba4dbe780482fda840a6912e8aa0cf2e4b2b4a
vinit-modi <vinitmodi.dev@gmail.com>
2023-10-23 22:09:04 -0400

    CMS schema

---
+++ server/db/models/Cms.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const DS = require("../../services/date");
+
+const CmsSchema = new mongoose.Schema({
+  titleKey: String,
+  titleValue: String,
+  content: String,
+  createdAt: { type: String, default: "" },
+  updatedAt: { type: String, default: "" },
+});
+
+CmsSchema.pre("save", function (next) {
+  if (!this.createdAt) {
+    this.createdAt = DS.now();
+  }
+  this.updatedAt = DS.now();
+  next();
+});
+
+CmsSchema.pre("findOneAndUpdate", function (next) {
+  this._update.updatedAt = DS.now();
+  next();
+});
+
+const UserModel = mongoose.model("cms", CmsSchema);
+
+module.exports = UserModel;
