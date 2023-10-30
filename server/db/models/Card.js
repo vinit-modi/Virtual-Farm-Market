8b73583e14e9146106bd6bbacffad18f05065483
vinit-modi <vinitmodi.dev@gmail.com>
2023-10-30 18:12:30 -0400

    updated card model

--- server/db/models/Card.js
+++ server/db/models/Card.js
@@ -1,17 +1,30 @@
 const mongoose = require("mongoose");
+const DS = require("../../services/date");
 
 const CreditCardSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   cardNumber: String,
   lastFourDigits: String,
   cardExpiration: String,
   cardType: String,
   cardholderName: String,
   cvv: String,
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  createdAt: { type: String, default: "" },
+  updatedAt: { type: String, default: "" },
 });
 
+CmsSchema.pre("save", function (next) {
+  if (!this.createdAt) {
+    this.createdAt = DS.now();
+  }
+  this.updatedAt = DS.now();
+  next();
+});
+
+CmsSchema.pre("findOneAndUpdate", function (next) {
+  this._update.updatedAt = DS.now();
+  next();
+});
 const CreditCardModel = mongoose.model("CreditCard", CreditCardSchema);
 
 module.exports = CreditCardModel;
