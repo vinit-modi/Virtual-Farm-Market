88d16617355a9fa1fe3a1622ccc9829189a490fb
vinit-modi <vinitmodi.dev@gmail.com>
2023-10-31 00:40:41 -0400

    Notification model

---
+++ server/db/models/Notification.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const DS = require("../../services/date");
+
+const NotificationSchema = new mongoose.Schema({
+  userId: String,
+  title: String,
+  content: String,
+  isRead: { type: Boolean, default: false },
+  isDeleted: { type: Boolean, default: false },
+  createdAt: { type: String, default: "" },
+  updatedAt: { type: String, default: "" },
+});
+
+NotificationSchema.pre("save", function (next) {
+  if (!this.createdAt) {
+    this.createdAt = DS.now();
+  }
+  this.updatedAt = DS.now();
+  next();
+});
+
+NotificationSchema.pre("findOneAndUpdate", function (next) {
+  this._update.updatedAt = DS.now();
+  next();
+});
+const NotificationModel = mongoose.model("notification", NotificationSchema);
+
+module.exports = NotificationModel;
