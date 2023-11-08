4e3121437df254e34f62b9658db80fb513252219
vinit-modi <vinitmodi.dev@gmail.com>
2023-11-08 16:39:01 -0500

    Only active categories will display

--- server/api/controller/Website/product.js
+++ server/api/controller/Website/product.js
@@ -28,7 +28,7 @@ module.exports = {
 
   categoriesForProduct: async (req, res) => {
     try {
-      let getCategories = await CategoryModel.find({});
+      let getCategories = await CategoryModel.find({ isActive: true });
       return res.status(200).json({
         status: "success",
         message: "All categories.",
