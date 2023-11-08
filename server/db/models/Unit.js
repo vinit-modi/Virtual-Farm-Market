6168f1e3e6ac2b55fb28f4f040623d31c7d14b10
vinit-modi <vinitmodi.dev@gmail.com>
2023-11-07 19:37:44 -0500

    Unit Model

---
+++ server/db/models/Unit.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const DS = require("../../services/date");
+
+const UnitSchema = new mongoose.Schema({
+  name: String,
+  createdAt: { type: String, default: "" },
+  updatedAt: { type: String, default: "" },
+});
+
+UnitSchema.pre("save", function (next) {
+  if (!this.createdAt) {
+    this.createdAt = DS.now();
+  }
+  this.updatedAt = DS.now();
+  next();
+});
+
+UnitSchema.pre("findOneAndUpdate", function (next) {
+  this._update.updatedAt = DS.now();
+  next();
+});
+
+const UserModel = mongoose.model("unit", UnitSchema);
+
+module.exports = UserModel;
