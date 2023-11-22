f140bd3f35c9b7f208c2a8beacc1bb5116fefe31
virenjagani <virenjagani0303@gmail.com>
2023-11-21 19:05:30 -0500

    Reusable Components

---
+++ client/Virtual-Farm-Market/src/components/Buttons/DefaultButton.jsx
@@ -0,0 +1,36 @@
+import { Button, Tooltip, Typography } from '@mui/material'
+import { green } from '@mui/material/colors'
+import React from 'react'
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+
+
+function DefaultButton() {
+  return (
+    <div>
+      <Tooltip title="Default Card">
+                                  <Button
+                                    disableElevation
+                                    disableFocusRipple
+                                    disableRipple
+                                    disableTouchRipple
+                                    sx={{
+                                      "&:hover": {
+                                        cursor: "default",
+                                      },
+                                    }}
+                                    startIcon={
+                                      <BookmarkIcon
+                                        sx={{
+                                          color: green["A700"],
+                                        }}
+                                      />
+                                    }
+                                  >
+                                    <Typography>Default</Typography>
+                                  </Button>
+                                </Tooltip>
+    </div>
+  )
+}
+
+export default DefaultButton
