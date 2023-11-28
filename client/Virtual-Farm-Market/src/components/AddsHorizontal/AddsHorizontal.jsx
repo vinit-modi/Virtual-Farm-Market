eae890781dc0a1099af230bd2c6a415dddce5d33
virenjagani <virenjagani0303@gmail.com>
2023-11-27 23:28:05 -0500

    Adds css edited

--- client/Virtual-Farm-Market/src/components/AddsHorizontal/AddsHorizontal.jsx
+++ client/Virtual-Farm-Market/src/components/AddsHorizontal/AddsHorizontal.jsx
@@ -6,19 +6,19 @@ import img3 from "../../Assets/adds/10003.jpg";
 
 function AddsHorizontal() {
   return (
-    <div style={{ marginBottom: "20PX" }}>
-      <div class="media-scroller snaps-inline">
-        <div class="media-elementd">
+    <div style={{ marginBottom: "20px" }}>
+      <div className="media-scroller snaps-inline">
+        <div className="media-elementd">
           <a
             href="https://www.instacart.ca/store/shoppers-drug-mart/storefront"
             target="_blank"
           >
-            <img src={img1} alt="" />
+            <img src={img1} alt="img1" className="responsive-img" />
           </a>
         </div>
-        <div class="media-elementd">
+        <div className="media-elementd">
           <a href="https://www.costco.ca/" target="_blank">
-            <img src={img2} alt="" />
+            <img src={img2} alt="img2" className="responsive-img" />
           </a>
         </div>
       </div>
