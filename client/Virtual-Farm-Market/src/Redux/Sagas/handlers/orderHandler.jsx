4a819cf4098bbd44b37a898b270b0ed07633254c
virenjagani <virenjagani0303@gmail.com>
2023-11-28 03:32:09 -0500

    Order setup

--- client/Virtual-Farm-Market/src/Redux/Sagas/handlers/orderHandler.jsx
+++ client/Virtual-Farm-Market/src/Redux/Sagas/handlers/orderHandler.jsx
@@ -1,17 +1,20 @@
 import { call, put } from "redux-saga/effects";
-import { failRequestOrder, makeRequestOrder, setAllOrder } from "../../Reducers/OrderReducer";
+import {
+  failRequestOrder,
+  makeRequestOrder,
+  setAllOrder,
+} from "../../Reducers/OrderReducer";
 import { requestAllOrder } from "../requests/orderRequest";
 
 export function* fetchAllOrder() {
-    try {
-      yield put(makeRequestOrder());
-      const response = yield call(requestAllOrder);
-  
-      response.status === 200
-        ? yield put(setAllOrder(response.data.data))
-        : yield put(failRequestOrder(response.data?.message));
-    } catch (error) {
-      yield put(failRequestOrder(error.message));
-    }
+  try {
+    yield put(makeRequestOrder());
+    const response = yield call(requestAllOrder);
+
+    response.status === 200
+      ? yield put(setAllOrder(response.data.data))
+      : yield put(failRequestOrder(response.data?.message));
+  } catch (error) {
+    yield put(failRequestOrder(error.message));
   }
-  
+}
--- client/Virtual-Farm-Market/src/Redux/Sagas/requests/orderRequest.jsx
+++ client/Virtual-Farm-Market/src/Redux/Sagas/requests/orderRequest.jsx
@@ -1,3 +1,4 @@
+import Axios from "../../../Utils/Axios";
 
 export function requestAllOrder() {
     const response = Axios.get("/api/getAllOrders");
